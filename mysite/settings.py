76ac16752d567d84b34f30cb63dfb8152c52cd60
JoYongHyun <jo4745@gmail.com>
2018-08-09 15:37:25 +0900

    호스트추가

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,8 +25,7 @@ SECRET_KEY = 'k(7o90r=4!qw77sm09u4lckn5fe^m)j$y2k5qo4p7-wmx+kn&4'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '[::1]', '.pythonanywhere.com']
 
 # Application definition
 
